a26fe472923617e6ce0ab19f03cdf735a1a4c29c
Nati Kamusher <natan.kamusher@gmail.com>
2025-06-08 18:42:03 +0300

    refactor: remove CoinContext and related provider files in favor of direct state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,8 @@
-import { CoinProvider } from "@context/CoinProvider";
 import "./App.css";
 import PortfolioPage from "@pages/PortfolioPage";
 
 function App() {
-  return (
-    <CoinProvider>
-      <PortfolioPage />
-    </CoinProvider>
-  );
+  return <PortfolioPage />;
 }
 
 export default App;
