a8b7f365327932b7ac81f21e15012e330999510d
Nati Kamusher <nati.kamusher@sage.com>
2025-10-13 20:30:41 +0300

    refactor(portfolio): import useCallback hook (T3.1)

--- frontend/src/pages/PortfolioPage.tsx
+++ frontend/src/pages/PortfolioPage.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { Toaster } from "react-hot-toast";
 
 import { AddAssetModal } from "@components/AddAssetModal";
